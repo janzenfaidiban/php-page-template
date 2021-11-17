342b867804cd08d58c826927aee4e5b4e8b6c766
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-11-17 22:11:17 +0900

    adding new pages

---
+++ profile.php
@@ -0,0 +1,22 @@
+<?php
+    // header
+    echo "<h1>SaCode</h1>";
+    echo "<p>#LearnToCode</p>";
+    echo "<hr>";
+    
+    // navigasi
+    echo "<a href='index.php'>Home</a> ";
+    echo "<a href='profile.php'>Profile</a> ";
+    echo "<a href='gallery.php'>Gallery</a> ";
+    echo "<a href='shop.php'>Shop</a> ";
+    echo "<a href='contact.php'>Contact</a> ";
+    
+    // main content
+    echo "<h2>Profile Page</h2>";
+    echo "<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, pariatur omnis atque aut iusto velit ea id quam? Quod asperiores corporis deserunt vitae temporibus assumenda porro, sunt ad dignissimos dolor.</p>";
+    
+    // footer
+    echo "<small>Copyright &copy; 2021 </small>";
+
+?>
+
